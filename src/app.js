bb11103edd17f5446ed16b11304d9764c78244cc
antfriend <antfriend@gmail.com>
2015-09-05 14:08:04 -0600

    initial stuff

---
+++ src/app.js
@@ -0,0 +1,55 @@
+/**
+ * 
+ * Blarfo!
+ */
+
+var ajax = require('ajax');
+var UI = require('ui');
+
+// Construct URL
+var cityName = 'Boise';
+var URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+var counterit = 0;
+
+// Create a Card with title and subtitle
+var card = new UI.Card({
+  title:'Weather',
+  subtitle:'Fetching...'
+  
+});
+
+// Make the request
+ajax(
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(data) {
+    // Success!
+    console.log('Successfully fetched weather data!');
+  
+    // Extract data
+    var location = data.name;
+    var temperature = Math.round(data.main.temp - 273.15) + 'C';
+  
+    // Always upper-case first letter of description
+    var description = data.weather[0].description;
+    description = description.charAt(0).toUpperCase() + description.substring(1);
+    // Show to user
+    card.subtitle(location + ', ' + temperature);
+    counterit++;
+    
+    card.body('count ' + counterit);
+    
+  },
+  function(error) {
+    // Failure!
+    console.log('Failed fetching weather data: ' + error);
+  }
+);
+
+
+// Display the Card
+card.show();
+
+
